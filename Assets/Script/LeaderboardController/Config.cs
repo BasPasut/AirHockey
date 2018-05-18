4a965ddd9a955b318a70b601127e35ce45c49f25
pawan1235 <pawan1235@hotmail.co.th>
2018-05-18 17:57:25 +0700

    bug fix

---
+++ Assets/Script/LeaderboardController/Config.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour {
+
+    private readonly string PRIVATEKEY = "ddQHR6_f4keHMODEUgC8WgCh9Z1BQEiE2C3yMABEDrhg" ;
+
+    public string PrivateKey {
+        get {
+            return PRIVATEKEY;
+        }
+    }
+
+}
